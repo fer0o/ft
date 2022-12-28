228ccf579aa341c4147daf7328cddfa937f92315
fer0o <fer0osennin@gmail.com>
2022-12-28 02:35:36 -0600

    firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCNbVBGWnjaGDa3KSK6BVr-9SDAP_clDGA',
+  authDomain: 'fort-b6990.firebaseapp.com',
+  projectId: 'fort-b6990',
+  storageBucket: 'fort-b6990.appspot.com',
+  messagingSenderId: '787669082820',
+  appId: '1:787669082820:web:5529f8aec8ebc30bdf9f91'
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+
+export default firebaseApp
